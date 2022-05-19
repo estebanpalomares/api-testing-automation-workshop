b12c92c9b36870858b9970d8c19af43abf588fba
Esteban Palomares <esteban.palomaresm@perficient.com>
2022-05-19 10:56:48 -0500

    Test Runner Set up

---
+++ src/test/java/training/api/test/stepdefinitions/APIMethodsStepDefinition.java
@@ -0,0 +1,9 @@
+package training.api.test.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class APIMethodsStepDefinition {
+    @Given("I have a Body Request")
+    public void iHaveABodyRequest() {
+    }
+}
---
+++ src/test/resources/features/APIMethodsTesting.feature
@@ -0,0 +1,7 @@
+@REGRESSION @API
+
+  @API_METHODS
+  Feature: Test the API methods
+
+    Scenario: Test GET method
+      Given I have a Body Request
